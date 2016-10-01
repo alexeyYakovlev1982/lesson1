637b9956077633c6375b1f262da1ea5ab0224863
Alexey Yakovlev <alexey.yakovlev1982@gmail.com>
2016-10-01 16:46:12 +0300

    count2 added

--- bot.py
+++ bot.py
@@ -5,14 +5,28 @@ def start(bot, update):
     print ("Вызван /start")
     bot.sendMessage(update.message.chat_id, text='Давай общаться!')
 
+############## count ########################
+
 def count(bot, update):
     print ("Вызван /count")
-    answer_text = 'Количество слов = ' + str(count_words(update.message.text) - 1)
+    answer_text = 'Количество слов=' + str(count_words(update.message.text) - 1)
     print ('/count ' + answer_text)
     bot.sendMessage(update.message.chat_id, text=answer_text)
 
+
 def count_words(message):
     return len(message.split())
+
+
+############## /count2 ########################
+
+def count2(bot, update):
+    print ("Вызван /count2")
+    bot.sendMessage(update.message.chat_id, text='Количество слов=' + str(len(update.message.text[7:]).split()))
+
+
+
+############## talk_to_me ########################
 
 def talk_to_me(bot, update):
     print('Пришло сообщение: %s' % update.message.text)
@@ -27,6 +41,7 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("count", count))
+    dp.add_handler(CommandHandler("count2", count))
     dp.add_handler(MessageHandler([Filters.text], talk_to_me))
 
     updater.start_polling()
